ca9709a6055e6ebec3a2fdb2bd2b4e16abfc4b26
Jitendra Patil <jipatil@microsoft.com>
2025-04-28 16:00:47 +0530

    Created new folder for torch code

--- Basics/text_encoding.py
+++ Basics/text_encoding.py
@@ -199,7 +199,7 @@ dataloader = create_dataloader_v1(
     raw_text, batch_size=8, max_length=max_length,
    stride=max_length, shuffle=False
 )
-  = iter(dataloader)
+data_iter  = iter(dataloader)
 inputs, targets = next(data_iter)
 print("Token IDs:\n", inputs)
 print("\nInputs shape:\n", inputs.shape)
